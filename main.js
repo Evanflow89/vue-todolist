693d227f7e45bc00acfda0c0b8742dca728f710e
Evanflow89 <ivanbaragone@gmail.com>
2022-05-23 15:59:15 +0200

    commit

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var app = new Vue({
     el: '#app',
     data: {
-
+        Todos: [
+            {
+                text: "Fare la spesa",
+                done: true;
+            },
+            {
+                text: "Workout",
+                done: true;
+            },
+            {
+                text: "Studiare",
+                done: true;
+            },
+            {
+                text: "Aggiustare il lavello",
+                done: true;
+            },
+        ]
     }
 })
